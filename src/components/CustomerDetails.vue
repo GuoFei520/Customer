5ed84c6fd07e55ba801809bedd8a2ea5ea035c63
guofei <18300852282@163.com>
2018-12-05 13:52:27 +0800

    更新项目

--- src/components/CustomerDetails.vue
+++ src/components/CustomerDetails.vue
@@ -2,32 +2,32 @@
   <div class="details container">
     <router-link to="/" class="btn btn-default">返回</router-link>
     <h1 class="page-header">
-        {{customer.name}}
-        <span class="pull-right">
-            <router-link v-bind:to="'/edit/' + customer.id" class="btn btn-primary">编辑</router-link>
-            <button class="btn btn-danger" v-on:click="deleteCustomer(customer.id)">删除</button>
-        </span>
+      {{customer.name}}
+      <span class="pull-right">
+        <router-link v-bind:to="'/edit/' + customer.id" class="btn btn-primary">编辑</router-link>
+        <button class="btn btn-danger" v-on:click="deleteCustomer(customer.id)">删除</button>
+      </span>
     </h1>
     <ul class="list-group">
       <li class="list-group-item">
-        <span class="glyphicon glyphicon-asterisk">{{customer.phone}}</span>
+        <i class="iconfont icon-weibiaoti"></i>&nbsp;{{customer.phone}}
       </li>
       <li class="list-group-item">
-        <span class="glyphicon glyphicon-plus">{{customer.email}}</span>
+        <i class="iconfont icon-youxiang"></i>&nbsp;{{customer.email}}
       </li>
     </ul>
     <ul class="list-group">
       <li class="list-group-item">
-        <span class="glyphicon glyphicon-asterisk">{{customer.education}}</span>
+        <i class="iconfont icon-xueli"></i>&nbsp;{{customer.education}}
       </li>
       <li class="list-group-item">
-        <span class="glyphicon glyphicon-plus">{{customer.graduationschool}}</span>
+        <i class="iconfont icon-daxue"></i>&nbsp;{{customer.graduationschool}}
       </li>
       <li class="list-group-item">
-        <span class="glyphicon glyphicon-asterisk">{{customer.profession}}</span>
+        <i class="iconfont icon-gongzuo"></i>&nbsp;{{customer.profession}}
       </li>
       <li class="list-group-item">
-        <span class="glyphicon glyphicon-plus">{{customer.profile}}</span>
+        <i class="iconfont icon-jianjie"></i>&nbsp;{{customer.profile}}
       </li>
     </ul>
   </div>
@@ -43,18 +43,15 @@ export default {
   },
   methods: {
     fetchCustomers(id) {
-      this.$http
-        .get("http://localhost:3004/users/" + id)
-        .then((response)=> {
-            this.customer = response.data;
-        });
+      this.$http.get("http://localhost:3004/users/" + id).then(response => {
+        this.customer = response.data;
+      });
     },
-    deleteCustomer(id){
-        // console.log(id);
-        this.$http.delete("http://localhost:3004/users/" + id)
-        .then((response)=>{
-            this.$router.push({path:"/",query:{alert:"用户删除成功!"}})
-        })
+    deleteCustomer(id) {
+      // console.log(id);
+      this.$http.delete("http://localhost:3004/users/" + id).then(response => {
+        this.$router.push({ path: "/", query: { alert: "用户删除成功!" } });
+      });
     }
   },
   created() {
